f8274962a08a440abfe4759f5b9aad78e7a6db09
mukul-truwix <mukuls@truwix.com>
2025-02-24 18:58:11 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,48 +1,48 @@
-import express from 'express'
-import cors from 'cors'
-import xlsx from "xlsx";
-import { Product } from "../src/model/product.model.js";
-import path from "path";
-import fs from "fs";
-const app = express()
+import express from "express";
+import cors from "cors";
+const app = express();
 
-const allowedOrigins = ["https://jaipur-jwellers-frontend.vercel.app", "http://localhost:5173","http://localhost:8000","https://jaipurjeweller.com"]; // Add any other origins you need
+const allowedOrigins = [
+  "https://jaipur-jwellers-frontend.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:8000",
+  "https://jaipurjeweller.com",
+  "https://jaipur-jewellers-backend.vercel.app",
+]; // Add any other origins you need
 
 app.use(
-    cors({
-        origin: function (origin, callback) {
-            // Allow requests with no origin (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                // If the origin isn't in the allowed list
-                return callback(new Error("Not allowed by CORS"), false);
-            }
-            return callback(null, true);
-        },
-        credentials: true, // if your frontend needs to send cookies
-    })
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If the origin isn't in the allowed list
+        return callback(new Error("Not allowed by CORS"), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // if your frontend needs to send cookies
+  })
 );
-app.use(express.json({ limit: '16kb' }))
-app.use(express.urlencoded({ extended: true, limit: '16kb' }))
-app.use(express.static('public'))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-app.get('/health', (req, res) => {
-    res.status(200).send('OK'); // Health check endpoint
+app.get("/health", (req, res) => {
+  res.status(200).send("OK"); // Health check endpoint
 });
 
 //import router here
-import userRouter from './routes/user.routes.js'
-import orderRouter from './routes/order.routes.js'
-import productRouter from './routes/product.routes.js'
-import adminRouter from './routes/admin.routes.js'
-import contactRoute from './routes/contact.routes.js'
+import userRouter from "./routes/user.routes.js";
+import orderRouter from "./routes/order.routes.js";
+import productRouter from "./routes/product.routes.js";
+import adminRouter from "./routes/admin.routes.js";
+import contactRoute from "./routes/contact.routes.js";
 
-app.use('/api/v1/contact',contactRoute)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/orders', orderRouter)
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/admin', adminRouter)
+app.use("/api/v1/contact", contactRoute);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/admin", adminRouter);
 
-
-
-export default app
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
-import dotenv from 'dotenv'
-import app from './app.js'
-import connectDB from './db/index.js'
-import morgan from 'morgan'
-import axios from 'axios'
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+import morgan from "morgan";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 
 connectDB()
-    .then(() => {
-        app.on("error", (error) => {
-            console.error("ERROR", error)
-            throw error
-        })
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 8000}`)
-            setInterval(async () => {
-                try {
-                    const response = await axios.get('https://jaipur-jwellers-backend.onrender.com/health')
-                    console.log(response.data)
-                } catch (error) {
-                    console.error('Health check failed:', error)
-                }
-            }, 5 * 60 * 1000)
-        })
-    })
-    .catch((error) => {
-        console.error("ERROR While connecting to Database: ", error)
-    })
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("ERROR", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("ERROR While connecting to Database: ", error);
+  });
